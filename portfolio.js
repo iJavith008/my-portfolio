c5fd34faa389c3f7261b704bac3be8b7695e12c2
iJavith008 <abdulsamedjavith@gmail.com>
2025-09-01 15:02:54 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+var typed= new Typed(".text", {
+    strings:["Frontend Developer" , "Student", "Web Developer"],
+    typespeed: 0.1,
+    backspeed: 5,
+    backDelay: 2000,
+    loop:true
+})
+
+console.log("test")
